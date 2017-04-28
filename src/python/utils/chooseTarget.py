a6c39d1cd0603c6c6e78a7e80b84aef65811022e
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-04-28 13:46:32 -0400

    no message

--- src/python/utils/chooseTarget.py
+++ src/python/utils/chooseTarget.py
@@ -16,7 +16,8 @@ def chooseTarget(TargetList，pixLocX, picLocY):
     hList = [i_target.h for i_target in TargetList]
     areaList = [w*h for w, h in zip(wList, hList)]
     distList = [sqrt(x**2+y**2) for x, y in zip(xList, yList)]
-    findTar = [weight * area/(dist+const) for area, dist in zip(areaList, distList)]
+    # findTar = [weight * area/(dist+const) for area, dist in zip(areaList, distList)]
+    findTar = areaList
     max_Tar = max(findTar)
     ind = [i for i in range(len(findTar)) if findTar[i] == max_Tar]
     return TargetList[ind[0]]
