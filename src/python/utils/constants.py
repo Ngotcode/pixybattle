9a78c1f6292067c38708421ad899533e9f73fa52
Yinan <wany@janelia.hhmi.org>
2017-05-03 22:00:39 +0500

    checkin code from the bot

--- src/python/utils/constants.py
+++ src/python/utils/constants.py
@@ -3,3 +3,28 @@ import os
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../..'))
 assert os.path.split(ROOT_DIR)[-1] == 'pixybattle'  # make sure the relative path works
+
+# defining PixyCam sensory variables
+PIXY_MIN_X = 0
+PIXY_MAX_X = 319
+PIXY_MIN_Y = 0
+PIXY_MAX_Y = 199
+
+PIXY_X_CENTER = ((PIXY_MAX_X - PIXY_MIN_X) / 2)
+PIXY_Y_CENTER = ((PIXY_MAX_Y - PIXY_MIN_Y) / 2)
+PIXY_RCS_MIN_POS = 0
+PIXY_RCS_MAX_POS = 1000
+PIXY_RCS_CENTER_POS = ((PIXY_RCS_MAX_POS - PIXY_RCS_MIN_POS) / 2)
+BLOCK_BUFFER_SIZE = 10
+
+# defining PixyCam motor variables
+PIXY_RCS_PAN_CHANNEL = 0
+PIXY_RCS_TILT_CHANNEL = 1
+
+PAN_PROPORTIONAL_GAIN = 400
+PAN_DERIVATIVE_GAIN = 300
+TILT_PROPORTIONAL_GAIN = 500
+TILT_DERIVATIVE_GAIN = 400
+
+MAX_MOTOR_SPEED = 300  # 480
+MIN_MOTOR_SPEED = -480
