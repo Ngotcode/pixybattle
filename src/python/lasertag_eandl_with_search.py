2e567ce2e1ce6227f68295f3ad66a9e4fb8eea05
Yinan Wan <Yinan Wan>
2017-05-03 18:17:30 -0400

    dopan 0

--- src/python/lasertag_eandl_with_search.py
+++ src/python/lasertag_eandl_with_search.py
@@ -232,7 +232,7 @@ def loop(robot_state):
     while not pixy.pixy_blocks_are_new() and run_flag:
         pass
     # count = pixy.pixy_get_blocks(BLOCK_BUFFER_SIZE, robot_state.blocks)
-    block = scan_scene(robot_state.blocks, 1)
+    block = scan_scene(robot_state.blocks, 0)
     if block == None:
         count = 0
     else:
