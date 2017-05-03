c75f81b42e162e4919c49dd5fe26fbc19368253f
Yinan <wany@janelia.hhmi.org>
2017-05-04 02:31:07 +0500

    leo new state-py file

--- src/python/utils/robot_state.py
+++ src/python/utils/robot_state.py
@@ -8,8 +8,8 @@ class RobotState(object):
         self.bias = 0
         self.advance = 0
         self.drive_gain = 1
-        self.h_pgain = 0.5
-        self.h_dgain = 0
+        self.h_pgain = 0.7
+        self.h_dgain = 0.2
         self.turn_error = 0
         self.current_time = current_time
         self.last_time = current_time
@@ -24,3 +24,5 @@ class RobotState(object):
         self.target_dist = 100
         self.ref_dist = 400
         self.previous_turn_error = 0
+        self.previous_time = current_time
+        self.deadband = 0.05 * total_drive
