58a88dfa45237f5013d5ce7c7a47fdc6cae0ab7c
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-05-04 13:46:44 -0400

    added pan state, and minor refactor

--- src/python/utils/constants.py
+++ src/python/utils/constants.py
@@ -10,24 +10,30 @@ assert os.path.split(ROOT_DIR)[-1] == 'pixybattle'  # make sure the relative pat
 
 # STATES
 
-class State(Enum):
+class Behaviour(Enum):
     """
-    Definition of robot's states (here for consistency). Add states where required. Integer values are meaningless, 
-    they just need to be different.
+    Definition of robot's behavioural states (here for consistency). Add states where required, but always refer to 
+    them through this static class for consistency.
     
     Examples
     --------
     Fictitious robot API for demonstrative purposes
-    >>> example_bot.set_state(State.SEARCH)
-    >>> if example_bot.state == State.SEARCH:
+    >>> example_bot.set_behaviour(Behaviour.SEARCH)
+    >>> if example_bot.behaviour == Behaviour.SEARCH:
     >>> ....example_bot.do_things()
     """
 
-    """Normal behaviour when looking for target"""
-    SEARCH = 0
+    IDLE = 'idle'
+    """Stationary, non-scanning, will not change unless state is changed"""
 
+    SEARCH = 'search'
+    """Looking for target while moving"""
+
+    APPROACH = 'approach'
     """Target acquired, go towards it while firing"""
-    APPROACH = 1
 
+    PAN = 'pan'
+    """Stationary, panning camera"""
+
+    PANIC = 'panic'
     """Everything has gone wrong, spin around and shoot"""
-    PANIC = 2
