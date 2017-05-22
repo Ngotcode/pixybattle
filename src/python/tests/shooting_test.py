34debca23f71bb15dccac97d4f0f02edd4b86a97
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-05-22 15:41:14 -0400

    failing tests for flags

--- src/python/tests/shooting_test.py
+++ src/python/tests/shooting_test.py
@@ -1,4 +1,4 @@
-from multiprocessing import Queue
+from multiprocessing import Process
 from multiprocessing.queues import Empty
 import time
 
@@ -6,6 +6,26 @@ import pytest
 from mock import Mock, patch
 
 from utils.shooting import DeadDrop, LaserCommander, LaserProcess
+
+
+class DeadDropTestProcess(Process):
+    def __init__(self, dead_drop, item):
+        super(DeadDropTestProcess, self).__init__()
+        self.dead_drop = dead_drop
+        self.item = item
+
+    def run(self):
+        pass
+
+
+class DeadDropTestPutProcess(DeadDropTestProcess):
+    def run(self):
+        self.dead_drop.put(self.item)
+
+
+class DeadDropTestGetProcess(DeadDropTestProcess):
+    def run(self):
+        assert self.dead_drop.get() == self.item
 
 
 # DEADDROP TESTS
@@ -41,3 +61,20 @@ def test_deaddrop_throw_empty(dead_drop):
     with pytest.raises(Empty):
         dead_drop.get()
 
+
+def test_deaddrop_get_across_processes(dead_drop):
+    item = 'item'
+    ddtp = DeadDropTestGetProcess(dead_drop, item)
+    ddtp.start()
+    time.sleep(0.01)
+    assert dead_drop.get() == item
+    ddtp.join()
+
+
+def test_deaddrop_put_across_processes(dead_drop):
+    item = 'item'
+    ddtp = DeadDropTestPutProcess(dead_drop, item)
+    dead_drop.put(item)
+    time.sleep(0.1)
+    ddtp.start()
+    ddtp.join()
