1e595fa38d7c9c0b52645e2a076d801491d6eb98
Yinan Wan <Yinan Wan>
2017-06-02 17:36:57 -0400

    change print

--- src/python/utils/scan_scene.py
+++ src/python/utils/scan_scene.py
@@ -15,7 +15,7 @@ def print_block_info(blocks):
     #     for index in range(0, count):
     #         print '[BLOCK_TYPE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d ANGLE=%3d]' % (blocks[index].type, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height, blocks[index].angle)
     for block in blocks:
-        print(str(object='block'))
+        print(str(block))
 
 def search_max_blocks(signature, blocks, count):
     for n_count in range(count):
