a23d4d218e0f6d61546f57e627ae881376d421a0
Yinan Wan <Yinan Wan>
2017-06-02 18:26:34 -0400

    area

--- src/python/utils/scan_scene.py
+++ src/python/utils/scan_scene.py
@@ -17,8 +17,9 @@ def print_block_info(blocks):
     for block in blocks:
         print(str(block))
 
-def search_max_blocks(signature, blocks, count):
-    for n_count in range(count):
+
+def search_max_blocks(signature, blocks):
+    for n_count in range(len(blocks)):
         if blocks[n_count].signature == signature and valid_block(blocks[n_count]):
             return blocks[n_count]
     return None
@@ -31,8 +32,25 @@ def valid_block(block):
 
 
 def area(block):
+    if block is None:
+        return 0
     return block.width * block.height
 
+
+def area_with_weight(block, weight):
+    return area(block) * weight
+
+
+def find_max_block_in_scene(blocks, signatures, weights):
+    best_block = None
+    max_area = 0
+    for sig, weight in zip(signatures, weights):
+        current_block = search_max_blocks(sig, blocks)
+        weighted_area = area_with_weight(current_block, weight)
+        if weighted_area > max_area:
+            best_block = current_block
+            max_area = weighted_area
+    return best_block, max_area
 
 
 def scan_scene(blocks, do_pan):
@@ -43,12 +61,11 @@ def scan_scene(blocks, do_pan):
     TODO: global variable "blocks" and constants not included in this function
     """
     # detect objects in the scene
-    block_with_signature = [None, None]
+    # block_with_signature = [None, None]
     tar_pan_view = -1
-    area_list = [0, 0]
-    target_signature = 1
+    tar_area = 0
+    tar_block = None
 
-    self_signature = 2  # save for future
 
     if do_pan:
         for pan_view in range(0, 1000, step_size):
@@ -56,29 +73,25 @@ def scan_scene(blocks, do_pan):
             pixy.pixy_rcs_set_position(PIXY_RCS_PAN_CHANNEL, pan_view)
             time.sleep(wait_time)
             blocks = PixyBlock.from_pixy()
-            count = len(blocks)
-            if count > 0:
-                print_block_info(blocks)
-                block = search_max_blocks(target_signature, blocks, count)
-                if block is None:
-                    continue
-                if area_list[target_signature - 1] < area(block):
-                    block_with_signature[target_signature - 1] = block
-                    area_list[target_signature - 1] = area(block)
-                    tar_pan_view = pan_view
-        # print(tar_pan_view)
+        # count = len(blocks)
+        # if count > 0:
+            #print_block_info(blocks)
+            block, max_area_pan = find_max_block_in_scene(blocks, SIGNATURE_LIST, TARGET_WEIGHT_MATRIX)
+            # if area_list[target_signature - 1] < area(block):
+            #     block_with_signature[target_signature - 1] = block
+            #     area_list[target_signature - 1] = area(block)
+            #     tar_pan_view = pan_view
+            if tar_area < max_area_pan:
+                tar_area = max_area_pan
+                tar_block = block
+                tar_pan_view = pan_view
+
+        print(tar_pan_view)
         if tar_pan_view < 0:
             tar_pan_view = PIXY_RCS_CENTER_POS
         pixy.pixy_rcs_set_position(PIXY_RCS_PAN_CHANNEL, tar_pan_view)
     else:
-            count = pixy.pixy_get_blocks(BLOCK_BUFFER_SIZE, blocks)
-            if count < 0:
-                print 'Error: pixy_get_blocks() [%d] ' % count
-                pixy.pixy_error(count)
-                sys.exit(1)
-            else:
-                if count > 0:
-                    # print_block_info(blocks, count)
-                    block_with_signature[0] = search_max_blocks(target_signature, blocks, count)
+        blocks = PixyBlock.from_pixy()
+        tar_block, _ = find_max_block_in_scene(blocks, SIGNATURE_LIST, TARGET_WEIGHT_MATRIX)
 
-    return block_with_signature[0]
+    return tar_block
