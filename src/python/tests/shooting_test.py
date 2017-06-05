6f2cb08622c5114faa616414cd7a901a859133f5
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-05 19:53:16 -0400

    shooting tests

--- src/python/tests/shooting_test.py
+++ src/python/tests/shooting_test.py
@@ -1,45 +1,32 @@
 import os
-from multiprocessing import Process, Queue
-from multiprocessing.queues import Empty
+from multiprocessing import Process
+import datetime
 import time
 import logging
 import csv
+import json
 
 import pytest
 
-from utils.shooting import get_with_default, put_singular, WeaponsOfficer
+from utils.shooting import LaserController, LaserInterface
 
 logger = logging.getLogger('tests.shooting_test')
 
-DEBUG = False
+DEBUG = True
 
 OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output', 'shooting_test')
-SHORT_COOLDOWN = 0.1  # seconds
+
+SHORT_COOLDOWN = 0.2  # seconds
+SHORT_RECOVERY = 0.3  # seconds
 
 READ = 'READ'
 WRITE = 'WRITE'
 FIRE = r'FIRE\n'
+HIT = 'HIT'
 
 
-class SingletonQueueTestProcess(Process):
-    def __init__(self, queue, item):
-        super(SingletonQueueTestProcess, self).__init__()
-        self.queue = queue
-        self.item = item
-
-    def run(self):
-        pass
-
-
-class SingletonQueueTestPutProcess(SingletonQueueTestProcess):
-    def run(self):
-        self.queue.put(None)
-        put_singular(self.queue, self.item)
-
-
-class SingletonQueueTestGetProcess(SingletonQueueTestProcess):
-    def run(self):
-        assert self.queue.get() == self.item
+INIT_VALUE = datetime.datetime(100, 1, 1)
+TEST_VALUE = datetime.datetime(200, 1, 1)
 
 
 def empty_output_dir():
@@ -63,86 +50,81 @@ def teardown_module():
         empty_output_dir()
 
 
+class ValueChangerProcess(Process):
+    def __init__(self, laser_interface):
+        super(ValueChangerProcess, self).__init__()
+        self.interface = laser_interface
+
+    def run(self):
+        self.interface.last_fired = TEST_VALUE
+
+
+class ValueCheckerProcess(Process):
+    def __init__(self, laser_interface):
+        super(ValueCheckerProcess, self).__init__()
+        self.interface = laser_interface
+        self.initial_val = laser_interface.last_fired
+
+    def run(self):
+        time.sleep(0.1)
+        assert self.initial_val == INIT_VALUE
+        assert self.interface.last_fired == TEST_VALUE
+
+
 # DEADDROP TESTS
 
 
 @pytest.fixture
-def singleton_queue():
-    return Queue(1)
+def laser_interface():
+    return LaserInterface(SHORT_COOLDOWN, SHORT_RECOVERY)
 
 
-def test_put_singular_single(singleton_queue):
-    assert singleton_queue.empty()
-    put_singular(singleton_queue, 'item')
-    assert singleton_queue.qsize() == 1
+def test_put_across_processes(laser_interface):
+    laser_interface.last_fired = INIT_VALUE
+    proc = ValueChangerProcess(laser_interface)
+    proc.start()
+    time.sleep(0.1)
+    assert laser_interface.last_fired == TEST_VALUE
+    proc.join()
 
 
-def test_put_singular_multiple(singleton_queue):
-    assert singleton_queue.empty()
-    singleton_queue.put('item')
-    item2 = 'another_item'
-    put_singular(singleton_queue, item2)
-    assert singleton_queue.qsize() == 1
-    assert singleton_queue.get() == item2
+def test_get_across_processes(laser_interface):
+    laser_interface.last_fired = INIT_VALUE
+    proc = ValueChangerProcess(laser_interface)
+    proc.start()
+    laser_interface.last_fired = TEST_VALUE
+    proc.join()
 
 
-def test_get_default_exists(singleton_queue):
-    assert singleton_queue.empty()
-    singleton_queue.put('item')
-    assert singleton_queue.qsize() == 1
-    output = get_with_default(singleton_queue, 'not_item')
-    assert output == 'item'
+def datetime_to_list(dt=None):
+    if dt is None:
+        dt = datetime.datetime.now()
+    return [dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.microsecond]
 
 
-def test_get_default_not_exists(singleton_queue):
-    assert singleton_queue.empty()
-    item = 'item'
-    output = get_with_default(singleton_queue, item)
-    assert output == item
-
-
-def test_get_default_throw_empty(singleton_queue):
-    with pytest.raises(Empty):
-        get_with_default(singleton_queue)
-
-
-def test_get_across_processes(singleton_queue):
-    item = 'item'
-    ddtp = SingletonQueueTestPutProcess(singleton_queue, item)
-    ddtp.start()
-    time.sleep(0.5)
-    assert singleton_queue.get() == item
-    ddtp.join()
-
-
-def test_put_across_processes(singleton_queue):
-    item = 'item'
-    ddtp = SingletonQueueTestGetProcess(singleton_queue, item)
-    singleton_queue.put(item)
-    ddtp.start()
-    ddtp.join()
+def list_to_datetime(lst):
+    return datetime.datetime(*lst)
 
 
 class DummySerial(object):
-    def __init__(self, output_filename=None, canned_responses=None):
-        self.canned_responses = dict() if canned_responses is None else canned_responses
-        self.next_response = ''
+    def __init__(self, output_filename=None, is_hit=False):
         self.output_path = self.set_filename(output_filename)
+        self.is_hit = is_hit
 
     def write(self, arg):
         self.log(WRITE, arg)
-        self.next_response = self.canned_responses.get(arg, '')
+
+    @property
+    def in_waiting(self):
+        return self.is_hit
 
     def read(self):
-        response = self.next_response
-        self.next_response = ''
-        self.log(READ, response)
-        return response
+        return HIT if self.is_hit else ''
 
     def log(self, operation, msg=''):
         with open(self.output_path, 'a') as f:
             f.write(
-                '{}\t{}\t{}\n'.format(time.time(), operation, msg.encode('string_escape') if msg else 'EMPTY_MESSAGE')
+                '{}\t{}\t{}\n'.format(datetime_to_list(), operation, msg.encode('string_escape') if msg else 'EMPTY_MESSAGE')
             )
 
     def set_filename(self, filename):
@@ -154,74 +136,79 @@ class DummySerial(object):
     def read_log(self):
         with open(self.output_path) as f:
             reader = csv.DictReader(f, fieldnames=['time', 'op', 'msg'], delimiter='\t')
-            return [line for line in reader]
+            out = []
+            for line in reader:
+                line['time'] = list_to_datetime(json.loads(line['time']))
+                out.append(line)
+            return out
 
 
 @pytest.fixture
-def cmdr_dummy(request):
-    commander = WeaponsOfficer()
+def controller_dummy(request):
+    controller = LaserController()
     dummy_serial = DummySerial(request.function.__name__ + '.txt')
-    commander.weapons_system.cooldown = SHORT_COOLDOWN
-    commander.weapons_system.ser = dummy_serial
-    return commander, dummy_serial
+    controller.interface.cooldown = datetime.timedelta(seconds=SHORT_COOLDOWN)
+    controller.interface.recovery = datetime.timedelta(seconds=SHORT_RECOVERY)
+    controller.laser_process.ser = dummy_serial
+    return controller, dummy_serial
 
 
-def test_instantiate_commander(cmdr_dummy):
-    cmdr, dummy_serial = cmdr_dummy
-    cmdr.start()
-    assert cmdr._laser.is_alive()
-    cmdr.stand_down()
-    assert not cmdr._laser.is_alive()
+def test_instantiate_controller(controller_dummy):
+    controller, dummy_serial = controller_dummy
+    controller.start()
+    assert controller.laser_process.is_alive()
+    controller.stand_down()
+    assert not controller.laser_process.is_alive()
 
 
-def test_commander_with(cmdr_dummy):
-    cmdr, dummy_serial = cmdr_dummy
-    with cmdr:
-        assert cmdr._laser.is_alive()
-    assert not cmdr._laser.is_alive()
+def test_commander_with(controller_dummy):
+    controller, dummy_serial = controller_dummy
+    with controller:
+        assert controller.laser_process.is_alive()
+    assert not controller.laser_process.is_alive()
 
 
-def test_fire_once(cmdr_dummy):
-    cmdr, dummy_serial = cmdr_dummy
-    with cmdr:
-        cmdr.fire_once()
+def test_fire_once(controller_dummy):
+    controller, dummy_serial = controller_dummy
+    with controller:
+        controller.fire_once()
     log = dummy_serial.read_log()
     assert len(log) == 1
     assert log[0]['op'] == WRITE
     assert log[0]['msg'] == FIRE
 
 
-def test_fire_multiple(cmdr_dummy):
-    cmdr, dummy_serial = cmdr_dummy
-    with cmdr:
-        cmdr.fire_multiple(3)
+def test_fire_multiple(controller_dummy):
+    controller, dummy_serial = controller_dummy
+    with controller:
+        controller.fire_multiple(3)
     log = dummy_serial.read_log()
     assert len(log) == 3
     assert all(line['op'] == WRITE for line in log)
     assert all(line['msg'] == FIRE for line in log)
     for this, that in zip(log, log[1:]):
-        assert float(that['time']) - float(this['time']) > SHORT_COOLDOWN
+        assert that['time'] - this['time'] >= datetime.timedelta(seconds=SHORT_COOLDOWN)
 
 
-def test_fire_at_will(cmdr_dummy):
-    cmdr, dummy_serial = cmdr_dummy
-    with cmdr:
-        cmdr.fire_at_will()
+def test_fire_at_will(controller_dummy):
+    controller, dummy_serial = controller_dummy
+    with controller:
+        controller.fire_at_will()
         time.sleep(SHORT_COOLDOWN*6)
     log = dummy_serial.read_log()
     assert len(log) > 3  # allow some time for loop execution etc.
     assert all(line['op'] == WRITE for line in log)
     assert all(line['msg'] == FIRE for line in log)
     for this, that in zip(log, log[1:]):
-        assert float(that['time']) - float(this['time']) > SHORT_COOLDOWN
+        assert that['time'] - this['time'] >= datetime.timedelta(seconds=SHORT_COOLDOWN)
 
 
-def test_last_fired(cmdr_dummy):
-    cmdr, dummy_serial = cmdr_dummy
-    with cmdr:
-        cmdr.fire_once()
-        first_shot = cmdr.last_fired
-        cmdr.fire_once()
-        second_shot = cmdr.last_fired
+def test_last_fired(controller_dummy):
+    controller, dummy_serial = controller_dummy
+    with controller:
+        controller.fire_once()
+        first_shot = controller.last_fired
+        controller.fire_once()
+        second_shot = controller.last_fired
 
-    assert second_shot - first_shot > SHORT_COOLDOWN
+    assert second_shot - first_shot > datetime.timedelta(seconds=SHORT_COOLDOWN)
