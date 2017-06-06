1209900422da94737a30971e97f036cdf550ffb1
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-06 18:46:39 -0400

    probabilistic tweeter

--- src/python/utils/bot_logging/twitter_logging.py
+++ src/python/utils/bot_logging/twitter_logging.py
@@ -3,10 +3,11 @@ import os
 import json
 import logging
 from io import BytesIO
+import random
 
 import tweepy
 
-from utils.constants import ROOT_DIR
+from utils.constants import ROOT_DIR, TWEET_PROB
 from utils.bot_logging.image_logging import ImageCreator
 
 logger = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ CREDENTIALS_PATH = os.path.join(ROOT_DIR, 'team4_keys.json')
 
 
 class Tweeter(object):
-    _instance = None
+    api = None
 
     def __init__(self, api=None):
         """
@@ -31,16 +32,16 @@ class Tweeter(object):
         """
         self.image_creator = ImageCreator()
 
-        if api is None:
-            with open(CREDENTIALS_PATH) as f:
-                cred = json.load(f)
+        if self.api is None:
+            if api is None:
+                with open(CREDENTIALS_PATH) as f:
+                    cred = json.load(f)
 
-            auth = tweepy.OAuthHandler(cred['consumer_key'], cred['consumer_secret'])
-            auth.set_access_token(cred['access_token'], cred['access_secret'])
+                auth = tweepy.OAuthHandler(cred['consumer_key'], cred['consumer_secret'])
+                auth.set_access_token(cred['access_token'], cred['access_secret'])
 
-            self.api = tweepy.API(auth)
-        else:
-            self.api = api
+                api = tweepy.API(auth)
+            type(self).api = api
 
     def tweet(self, msg):
         """Tweet the given message. The message is not validated for length etc."""
@@ -52,12 +53,20 @@ class Tweeter(object):
         buf.seek(0)
         self.api.update_with_media('image.png', status=msg, file=buf)
 
-    @classmethod
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = object.__new__(cls)
 
-        return cls._instance
+class ProbabilisticTweeter(Tweeter):
+    def __init__(self, api=None, default_prob=TWEET_PROB, seed=None):
+        super(ProbabilisticTweeter, self).__init__(api)
+        self.random = random.Random(seed)
+        self.default_prob = default_prob
+
+    def tweet(self, msg, p=None):
+        if self.random.random() < (p if p is None else self.default_prob):
+            super(ProbabilisticTweeter, self).tweet(msg)
+
+    def tweet_blocks(self, blocks, msg='', title=None, p=None, **kwargs):
+        if self.random.random() < (p if p is None else self.default_prob):
+            super(ProbabilisticTweeter, self).tweet_blocks(blocks, msg='', title=None, **kwargs)
 
 
 if __name__ == '__main__':
