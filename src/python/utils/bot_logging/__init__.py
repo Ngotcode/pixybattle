02cd3a33343a90b5d146b5e050ab67a8a60c3323
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-23 18:29:02 -0400

    add dummy api to bot logging init

--- src/python/utils/bot_logging/__init__.py
+++ src/python/utils/bot_logging/__init__.py
@@ -1,4 +1,4 @@
 from logging_constants import LOG_DIR
 from text_logging import set_log_level
-from twitter_logging import Tweeter
+from twitter_logging import Tweeter, DummyTweepyApi
 from image_logging import ImageLogger, ImageCreator
